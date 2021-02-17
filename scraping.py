96c4beaefcb8474fe2c945677efd980ceee01109
Lukasz Szymanski <Luk_szym@wp.pl>
2021-02-17 22:19:09 +0100

    Scraping collect_data method created

--- scraping.py
+++ scraping.py
@@ -1,20 +1,22 @@
 import requests
-import typing
 
 from bs4 import BeautifulSoup
+from time import sleep
 
 
 class ScrapeData:
     """Scrape web data class
     ----------------------
     Attributes:
+        last_date - the date of last record
     _______________________
     Methods:
         """
 
-    def __init__(self):
+    def __init__(self, last_date='2020-12-31'):
         self._www = 'https://policja.pl/pol/form/1,Informacja-dzienna.html'
-        self._table_class = 'table.table-listing:nth-child(2)'
+        self._table_class = 'table-listing table-striped margin_b20'
+        self.last_date = last_date
 
     def connection(self, page=0) -> requests.models.Response:
         """Create connection to specific webpage"""
@@ -24,6 +26,7 @@ class ScrapeData:
 
     @staticmethod
     def get_soup(res: requests.models.Response) -> BeautifulSoup:
+        """Create BS4 soup with lxml parsing"""
         soup = BeautifulSoup(res.text, 'lxml')
         return soup
 
@@ -31,5 +34,24 @@ class ScrapeData:
         """Get the proper data and divide it to list of days"""
         data = soup.find("table", {"class": self._table_class}).find_all('td')
         data = [item.text for item in data]
-        final = [data[i:i + 8] for i in range(0, len(data), 8)]
-        return final
+        data = [data[i:i + 8] for i in range(0, len(data), 8)]
+        return data
+
+    def collect_data(self) -> list:
+        """Collect daily reports from the last date to the newest available date"""
+        collected_data = []
+        page = 0
+        while True:
+            res = self.connection(page)
+            data = self.get_data(self.get_soup(res))
+            days = [row[0] for row in data]
+            print(f'Subpage {page + 1} opened, data loaded')
+            if self.last_date in days:
+                collected_data.extend(data[:days.index(self.last_date)])
+                print('End of loading data process.')
+                break
+            page += 1
+            sleep(0.5)
+            collected_data.extend(data)
+        collected_data.reverse()
+        return collected_data
